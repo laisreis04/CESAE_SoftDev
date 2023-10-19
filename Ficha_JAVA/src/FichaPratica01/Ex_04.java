d3b8707369e706b243344a5312704968cb62d044
Lais Reis <laisreis04@gmail.com>
2023-10-19 21:53:24 +0100

    New exercises
    Fiz mais dois exercícios, ainda falta corrigir o 06, não ficou bom ainda

--- Ficha_JAVA/src/FichaPratica01/Ex_04.java
+++ Ficha_JAVA/src/FichaPratica01/Ex_04.java
@@ -1,5 +1,24 @@
 package FichaPratica01;
 
+import java.util.Scanner;
+
 public class Ex_04 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        //Valores das variaves
+        double area, raio, pi = 3.1415;
+
+        //Ler variavel
+        System.out.print("Indique o valor: ");
+        raio = input.nextDouble();
+
+        // Calcular
+        area = pi * (raio*raio);
+
+        //Apresentar Resultado (imprimir)
+        System.out.print("Área: " + area);
+    }
 
 }
---
+++ Ficha_JAVA/src/FichaPratica01/Ex_06.java
@@ -0,0 +1,29 @@
+package FichaPratica01;
+
+import java.util.Scanner;
+
+public class Ex_06 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //Declarar variaveis
+        int valueA, valueB;
+
+        //Ler variaveis
+        valueB = valueA;
+        valueA = valueB;
+        System.out.print("Valor B: ");
+
+        System.out.print("Valor A: ");
+        valueA = valueB;
+
+
+
+        //calcular
+
+        //print
+
+
+    }
+}
