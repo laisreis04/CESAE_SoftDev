95d5b47242f17ff1e0e505bf2a2dc6eb2a457cc4
Lais Reis <laisreis04@gmail.com>
2023-10-25 10:13:55 +0100

    Update FichaPrática

--- Ficha_JAVA/src/FichaPratica02/Ex_18.java
+++ Ficha_JAVA/src/FichaPratica02/Ex_18.java
@@ -7,7 +7,7 @@ public class Ex_18 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int  dias, pagar = 0;
+        int  dias;
 
         System.out.print("Insira os dias que trabalhou esse mês: ");
         dias = input.nextInt();
@@ -17,15 +17,43 @@ public class Ex_18 {
 
         //valor iliquido a receber
 
-        switch (pagar){
-            case 'E':
-                System.out.print("Valor iliquido a receber é: " + dias*40*5 ); //A
-                System.out.print("\nTotal do subsídio de alimentação: " + dias*5 ); //B
+        switch (cargo){
+            case "E":
+                System.out.print("Valor iliquido a receber é: " + dias*40*5 + "€"); //A
+                System.out.print("\nTotal do subsídio de alimentação: " + dias*5 + "€"); //B
                 if (dias*40*5 < 1000){  //C
-                    System.out.print("\nRetenção de IRS: " + dias*40*5*0.1 );
+                    System.out.print("\nRetenção de IRS: " + dias*40*5*0.1 + "€");
+                }else {
+                    System.out.print("\nRetenção de IRS: " + dias*40*5*0.2 + "€");
                 }
-                System.out.print("\nSegurança Social: " + dias*40*5*0.3475 ); //D
-                System.out.print("\nValor líquido: " + dias*40*5*0.3475*0.1 ); //D
+                System.out.print("\nSegurança Social: " + dias*40*5*0.3475 + "€"); //D
+                System.out.print("\nValor líquido: " + (dias*40*5 - dias*40*5*0.3475*0.1) + "€"); //E
+                break;
+
+            case "C":
+                System.out.print("Valor iliquido a receber é: " + dias*60*7.5 + "€"); //A
+                System.out.print("\nTotal do subsídio de alimentação: " + dias*7.5 + "€"); //B
+                if (dias*60*7.5 < 1000){  //C
+                    System.out.print("\nRetenção de IRS: " + dias*60*7.5*0.1 + "€");
+                }else {
+                    System.out.print("\nRetenção de IRS: " + dias*60*7.5*0.2 + "€");
+                }
+                System.out.print("\nSegurança Social: " + dias*60*7.5*0.3475 + "€" ); //D
+                System.out.print("\nValor líquido: " + (dias*60*7.5 - dias*60*7.5*0.3475*0.2) + "€"); //E
+                break;
+
+            case "A":
+                System.out.print("Valor iliquido a receber é: " + dias*80*7.5 + "€"); //A
+                System.out.print("\nTotal do subsídio de alimentação: " + dias*7.5 + "€" ); //B
+                if (dias*80*7.5 < 1000){  //C
+                    System.out.print("\nRetenção de IRS: " + dias*80*7.5*0.1+ "€" );
+                }else {
+                    System.out.print("\nRetenção de IRS: " + dias*80*7.5*0.1 + "€");
+                }
+                System.out.print("\nSegurança Social: " + dias*80*5*0.30 + "€"); //D
+                System.out.print("\nValor líquido: " + (dias*80*7.5 - dias*80*7.5*0.30*0.2) + "€"); //E
+                break;
+
         }
 
     }
