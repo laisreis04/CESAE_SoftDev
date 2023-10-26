8a5bf693dfb28b5f065299627c11d169a583aad5
Lais Reis <laisreis04@gmail.com>
2023-10-26 13:26:32 +0100

    Update Ex_03

--- Ficha_JAVA/src/FichaExtra/Ex_03.java
+++ Ficha_JAVA/src/FichaExtra/Ex_03.java
@@ -2,7 +2,7 @@ package FichaExtra;
 
 import java.util.Scanner;
 
-public class Ex_02 {
+public class Ex_03 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
