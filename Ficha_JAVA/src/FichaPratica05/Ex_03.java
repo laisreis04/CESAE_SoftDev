1d3677a7149c4974d0d89da1136a1997fe5d4fe2
Lais Reis <laisreis04@gmail.com>
2023-11-03 12:42:40 +0000

    Update FichaPratica 05
    Resolução de novos exercícios.

---
+++ Ficha_JAVA/src/FichaPratica05/Ex_03.java
@@ -0,0 +1,28 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class Ex_03 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] array = new int[10];
+        int arrayNext = 0;
+
+        for(int i = 0; i < 10;i++){
+            System.out.print("Insira os números(" + i + "): ");
+            array[i] = input.nextInt();
+            arrayNext = array[i];
+            }
+        for(int x = 0; x < 10; x++){
+            System.out.println("O maior número é: " + arrayNext);
+            }
+
+
+
+
+        }
+    }
+
