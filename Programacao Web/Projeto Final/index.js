0e35ab2e001e781fa04254f44826c076314230f6
Lais Reis <laisreis04@gmail.com>
2023-11-10 16:38:57 +0000

    Update Projeto Web Final

---
+++ Programacao Web/Projeto Final/index.js
@@ -0,0 +1,19 @@
+//Acordion//
+
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionHeaders = document.querySelectorAll(".accordion-header");
+  
+    accordionHeaders.forEach(header => {
+      header.addEventListener("click", function() {
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          accordionHeaders.forEach(header => {
+            header.nextElementSibling.style.display = "none";
+          });
+          content.style.display = "block";
+        }
+      });
+    });
+  });
