dbc8c599d14cfd4bf36d6b061d128356ae10ed78
Lais Reis <laisreis04@gmail.com>
2024-01-16 13:06:35 +0000

    Angular

---
+++ Angular/AngularProject/src/app/Page/page-index/page-index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-index',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-index.component.html',
+  styleUrl: './page-index.component.css'
+})
+export class PageIndexComponent {
+
+}
